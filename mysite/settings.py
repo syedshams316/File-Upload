bcf720cdaf102275e7592a269cfa93c2c9b28f26
Shamsuddin Sayed <syedshams316@gmail.com>
2022-05-02 12:06:40 +0530

    Modified Settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@fba&@*8-n)rja37(%u$q_lm$538#_2erq)u@k3(7abxll06*k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
